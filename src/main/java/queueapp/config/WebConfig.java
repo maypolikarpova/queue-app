91f76ec3abf64e773b6c9899bc42a746f8c82a0e
may <mpo@adorsys.com.ua>
2018-12-08 15:09:21 +0200

    added cors configs

--- src/main/java/queueapp/config/WebConfig.java
+++ src/main/java/queueapp/config/WebConfig.java
@@ -1,4 +1,35 @@
 package queueapp.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class WebConfig {
+
+    private final CorsConfigProperties corsConfigProperties;
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.applyPermitDefaultValues();
+        config.setAllowCredentials(corsConfigProperties.getAllowCredentials());
+        config.setAllowedOrigins(corsConfigProperties.getAllowedOrigins());
+        config.setAllowedHeaders(corsConfigProperties.getAllowedHeaders());
+        config.setAllowedMethods(corsConfigProperties.getAllowedMethods());
+        config.setMaxAge(corsConfigProperties.getMaxAge());
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new FilterRegistrationBean(new CorsFilter(source));
+    }
 }
